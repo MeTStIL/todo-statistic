19c8447e351e16f0e72c48a86d6af347cc892066
MeTStIL <stme560@gmail.com>
2025-03-04 15:54:22 +0500

    first

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const {readLine} = require('./console');
 
 const files = getFiles();
 
+//console.log(require)
 console.log('Please, write your command!');
 readLine(processCommand);
 
@@ -16,10 +17,28 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+
+        case 'show':
+            for (const e of files) {
+                const linesInFile = e.split('\r\n');
+                for (line of linesInFile) {
+                    const index = isComment(line)
+                    if (index !== -1) {
+                        console.log(line.slice(index));
+                    }
+                }
+
+            }
+            break;
         default:
             console.log('wrong command');
             break;
     }
+}
+
+function isComment (str) {
+    const strTrim = str.trim();
+    return str.indexOf('// TODO ');
 }
 
 // TODO you can do it!
